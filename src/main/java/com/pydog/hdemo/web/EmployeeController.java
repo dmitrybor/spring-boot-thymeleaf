67f6316be7fd7b6dbd9deb655a68118890933964
Dmitry Borisenko <borisenkodv@gmail.com>
2022-09-08 12:22:05 +0600

    Add: basic table and bootstrap styles

---
+++ src/main/java/com/pydog/hdemo/web/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.pydog.hdemo.web;
+
+import com.pydog.hdemo.entity.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private List<Employee> employees;
+
+    @GetMapping("/list")
+    public String getEmployeeList(Model model) {
+        model.addAttribute("employees", employees);
+        return "list-employees";
+    }
+
+    @PostConstruct
+    private void loadData() {
+        this.employees = List.of(
+                new Employee(1, "Dmitry", "Borisenko", "dmitry@mailserver.com"),
+                new Employee(1, "Emma", "Smith", "emma@mailserver.com"),
+                new Employee(1, "Robert", "Burton", "rob@mailserver.com")
+        );
+    }
+}
