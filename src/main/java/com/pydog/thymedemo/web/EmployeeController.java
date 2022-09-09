69726d5dd35f7d1c6225f8dd1d24e22da83ffd77
Dmitry Borisenko <borisenkodv@gmail.com>
2022-09-09 12:05:10 +0600

    Add: update database data functionality

--- src/main/java/com/pydog/thymedemo/web/EmployeeController.java
+++ src/main/java/com/pydog/thymedemo/web/EmployeeController.java
@@ -6,10 +6,7 @@ import com.pydog.thymedemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/employees")
@@ -39,5 +36,12 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute Employee employee) {
         employeeService.save(employee);
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/employeeFormForUpdate")
+    public String showFormForUpdate(Model model, @RequestParam("employeeId") int id) {
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+        return "employeeForm";
     }
 }
